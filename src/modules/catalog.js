38c5476f49784338d88d4e86f1f8ce70d0eea16c
Sergei MILITER <militer@htmlcssjs.pro>
2021-12-19 23:40:52 +1000

    ADVANCED TASK 3.1

--- src/modules/catalog.js
+++ src/modules/catalog.js
@@ -1,13 +1,17 @@
 'use strict';
 import getData from './getData';
 import renderGoods from './renderGoods';
-import {categoryFilter} from './filters';
+import {categoryFilter, priceFilter} from './filters';
 
 const catalog = () => {
     const btnCatalog = document.querySelector('.catalog-button>button');
     const catalogModal = document.querySelector('.catalog');
     const catalogItems = catalogModal.querySelectorAll('li');
+    const $min = document.getElementById('min');
+    const $max = document.getElementById('max');
+
     let isOpen = false;
+
     btnCatalog.addEventListener('click', event => {
         isOpen = !isOpen;
         catalogModal.style.display = isOpen ? 'block' : '';
@@ -20,6 +24,18 @@ const catalog = () => {
                 renderGoods(categoryFilter(data, text));
             });
         });
+    });
+
+
+    [$min, $max].forEach(item => {
+        item.addEventListener('input', event => {
+            const min = $min.value ? $min.value : 0;
+            const max = $max.value ? $max.value : 100000;
+            getData().then(data => {
+                renderGoods(priceFilter(data, min, max));
+            });
+        });
+
     });
 };
 
